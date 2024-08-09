400ec33496147f8c3302c1fd2008d7feb563455b
leejongsuu <lik9819@naver.com>
2024-08-09 18:03:36 +0900

    basic-RequestCache-SavedRequest

--- src/main/java/security/demo/HomeController.java
+++ src/main/java/security/demo/HomeController.java
@@ -1,6 +1,10 @@
 package security.demo;
 
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +29,24 @@ public class HomeController {
     @GetMapping("/failed")
     public String fail() {
         return "fail";
+    }
+
+    @GetMapping("/anonymous")
+    public String anonymous() {
+        return "anonymous";
+    }
+
+    @GetMapping("/authentication")
+    public String authentication(Authentication authentication) {
+        if (authentication instanceof AnonymousAuthenticationToken) {
+            return "anonymous";
+        } else {
+            return "not anonymous";
+        }
+    }
+
+    @GetMapping("/anonymousContext")
+    public String anonymousContext(@CurrentSecurityContext SecurityContext securityContext) {
+        return securityContext.getAuthentication().getName();
     }
 }
--- src/main/java/security/demo/SecurityConfig.java
+++ src/main/java/security/demo/SecurityConfig.java
@@ -1,16 +1,23 @@
 package security.demo;
 
-
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
+
+import java.io.IOException;
 
 @EnableWebSecurity
 @Configuration
@@ -18,13 +25,25 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .httpBasic(basic -> basic.authenticationEntryPoint(new CustomerAuthenticationEntryPoint()));
 
+        HttpSessionRequestCache requestCache = new HttpSessionRequestCache();
+
+        http.authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated())
+                .formLogin(form -> form
+                        .successHandler(new AuthenticationSuccessHandler() {
+                            @Override
+                            public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                                SavedRequest savedRequest = requestCache.getRequest(request, response);
+                                String redirectUrl = savedRequest.getRedirectUrl();
+                                response.sendRedirect(redirectUrl);
+                            }
+                        }));
 
         return http.build();
     }
 
+    @Bean
     public UserDetailsService userDetailsService() {
         // UserDetailsService 대신 InMemoryUserDetailsManager도 가능
         UserDetails user1 = User.withUsername("user1")
