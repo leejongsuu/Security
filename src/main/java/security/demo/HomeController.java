51aa9c802bded2110a1f5341b110d1f251ce7f6b
leejongsuu <lik9819@naver.com>
2024-08-12 21:13:24 +0900

    SecurityContext

--- src/main/java/security/demo/HomeController.java
+++ src/main/java/security/demo/HomeController.java
@@ -1,14 +1,27 @@
 package security.demo;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    @Autowired
+    private SecurityContextService securityContextService;
+
     @GetMapping("/")
     public String index() {
+        SecurityContext context = SecurityContextHolder.getContextHolderStrategy().getContext();
+        Authentication authentication = context.getAuthentication();
+        System.out.println("authentication = " + authentication);
+
+        securityContextService.SecurityContext();
+
         return "index";
     }
 
--- src/main/java/security/demo/SecurityConfig.java
+++ src/main/java/security/demo/SecurityConfig.java
@@ -1,9 +1,9 @@
 package security.demo;
 
 
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -19,12 +19,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .httpBasic(basic -> basic.authenticationEntryPoint(new CustomerAuthenticationEntryPoint()));
-
+                .formLogin(Customizer.withDefaults());
 
         return http.build();
     }
 
+    @Bean
     public UserDetailsService userDetailsService() {
         // UserDetailsService 대신 InMemoryUserDetailsManager도 가능
         UserDetails user1 = User.withUsername("user1")
