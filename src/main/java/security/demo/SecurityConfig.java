2ffc2a889a3fdb8869006990bd9044fc2a00c696
leejongsuu <lik9819@naver.com>
2024-08-26 17:41:20 +0900

    UserDetailService

--- src/main/java/security/demo/SecurityConfig.java
+++ src/main/java/security/demo/SecurityConfig.java
@@ -3,15 +3,10 @@ package security.demo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -21,31 +16,29 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        AuthenticationManagerBuilder builder = http.getSharedObject(AuthenticationManagerBuilder.class);
-        builder.authenticationProvider(new CustomAuthenticationProvider());
-        builder.authenticationProvider(new CustomAuthenticationProvider2());
-
         http.authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults());
-//                .authenticationProvider(new CustomAuthenticationProvider())
-//                .authenticationProvider(new CustomAuthenticationProvider2());
         return http.build();
     }
 
     @Bean
     public UserDetailsService userDetailsService() {
-        // UserDetailsService 대신 InMemoryUserDetailsManager도 가능
-        UserDetails user1 = User.withUsername("user1")
-                .password("{noop}123")
-                .authorities("USER")
-                .build();
-
-        UserDetails user2 = User.withUsername("user2")
-                .password("{noop}123")
-                .authorities("USER")
-                .build();
-        return new InMemoryUserDetailsManager(user1, user2);
+        return new CustomUserDetailService();
     }
+
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        // UserDetailsService 대신 InMemoryUserDetailsManager도 가능
+//        UserDetails user1 = User.withUsername("user1")
+//                .password("{noop}123")
+//                .authorities("USER")
+//                .build();
+//
+//        UserDetails user2 = User.withUsername("user2")
+//                .password("{noop}123")
+//                .authorities("USER")
+//                .build();
+//        return new InMemoryUserDetailsManager(user1, user2);
+//    }
 }
