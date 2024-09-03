58d1023f11604c2f44bc6d8166c927fd4d3f5c0e
leejongsuu <lik9819@naver.com>
2024-09-03 10:47:16 +0900

    세션생성정책

--- src/main/java/security/demo/SecurityConfig.java
+++ src/main/java/security/demo/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -22,8 +23,7 @@ public class SecurityConfig {
         http.authorizeHttpRequests(auth -> auth
                         .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
-                .sessionManagement(session -> session.sessionFixation(sessionFixation -> sessionFixation.changeSessionId())
-                );
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED));
 
         return http.build();
     }
